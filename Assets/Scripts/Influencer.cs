60f9d53f7ee278d923a89c6215e343cf35dca36b
Ben Beverly <ben.beverly@krush.com>
2017-01-20 23:16:41 -0500

    Starting Node and Influencer scripts.

---
+++ Assets/Scripts/Influencer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influencer : MonoBehaviour {
+
+	[SerializeField]
+	private int influenceWeight = 1;
+	public int InfluenceWeight {
+		get {
+			return influenceWeight;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void EmitInfluence() {
+		// TODO fire particle effect
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+//	[SerializeField]
+//	private Player owner;
+
+	[SerializeField]
+	private int topHealth = 10;
+	public int TopHealth {
+		get {
+			return topHealth;
+		}
+	}
+
+	[SerializeField]
+	private int healthThreshold = 7;
+	public int HealthThreshold {
+		get {
+			return healthThreshold;
+		}
+	}
+
+	[SerializeField]
+	private int currentHealth = 0; // Current health 0 denotes a neutral node
+	public int CurrentHealth {
+		get {
+			return currentHealth;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
